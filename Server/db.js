15fc269c80241e07d5ce5dbc2bcfd1e345f6cd88
Muhamad Abdullah Iftikhar <120156630+M-Abdullah-Iftikhar@users.noreply.github.com>
2024-12-10 22:39:22 +0500

    hye i am updated

--- Server/db.js
+++ Server/db.js
@@ -3,7 +3,7 @@ const mongoose =  require('mongoose')
 // Function to connect with Database......................................
 
 const connecttomongoose = () => {
-    return mongoose.connect("mongodb+srv://muhammadabdullahmughal740:12341234@clusterlast.h3mhf.mongodb.net/")
+    return mongoose.connect("mongodb+srv://hamzanaseer496:y1E85MjLN8kl3lzj@cluster0.0cdiu.mongodb.net/")
 }
 
 // y1E85MjLN8kl3lzj
